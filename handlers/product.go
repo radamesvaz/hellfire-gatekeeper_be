6b607506414c8374e0cf0cac3785ccd56519098c
radamesvaz <rgvtovar@hotmail.com>
2025-03-18 02:27:00 -0400

    adding getting single product endpoint

--- handlers/product.go
+++ handlers/product.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	productsRepository "github.com/radamesvaz/bakery-app/repository/products"
 )
 
@@ -14,10 +16,28 @@ type ProductHandler struct {
 func (h *ProductHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	products, err := h.Repo.GetAll()
 	if err != nil {
-		http.Error(w, "Error obteniendo productos", http.StatusInternalServerError)
+		http.Error(w, "Error getting products", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(products)
+}
+
+func (h *ProductHandler) GetProductByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idProduct, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.Repo.GetProductByID(idProduct)
+	if err != nil {
+		http.Error(w, "Error getting product", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
 }
--- repository/products/product.go
+++ repository/products/product.go
@@ -40,3 +40,35 @@ func (r *ProductRepository) GetAll() ([]pModel.Product, error) {
 	}
 	return products, nil
 }
+
+func (r *ProductRepository) GetProductByID(idProduct uint64) (pModel.Product, error) {
+	fmt.Printf(
+		"Getting product by id = %v",
+		idProduct,
+	)
+
+	product := pModel.Product{}
+
+	err := r.DB.QueryRow(
+		"SELECT id_product, name, description, price, available, created_on FROM products WHERE id_product = ?",
+		idProduct,
+	).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Price,
+		&product.Available,
+		&product.CreatedOn,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Product not found")
+			return product, nil
+		}
+		fmt.Printf("Error retrieving the product: %v", err)
+		return product, nil
+	}
+
+	return product, nil
+}

726868a0a5fc94ce0d383881d432d8f738c6579c
radamesvaz <rgvtovar@hotmail.com>
2025-04-01 13:55:46 -0400

    adding run.sh

---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e  # Stop on error
+
+# Colors
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+NC="\033[0m" # No Color
+
+# Run all unit tests
+unit() {
+  echo -e "${YELLOW}🧪 Running unit tests...${NC}"
+  go clean -testcache
+    
+    start=$(date +%s)
+
+  if go test ./internal/...; then
+    end=$(date +%s)
+    runtime=$((end - start))
+    echo -e "${GREEN}✅ All UNIT tests passed in ${runtime}s${NC}"
+  fi
+}
+
+# Run all integration tests
+integration() {
+  echo -e "${YELLOW}🌐 Running integration tests...${NC}"
+  go clean -testcache
+
+      start=$(date +%s)
+
+  if go test -timeout 2m -v ./tests; then
+    end=$(date +%s)
+    runtime=$((end - start))
+    echo -e "${GREEN}✅ All INTEGRATION tests passed in ${runtime}s${NC}"
+  fi
+}
+
+# Run all tests
+tests() {
+  unit && integration
+}
+
+case "$1" in
+  unit)
+    unit
+    ;;
+  integration)
+    integration
+    ;;
+  tests)
+    tests
+    ;;
+  *)
+    echo -e "${RED}⚠️  Command not recognized: $1${NC}"
+    echo -e "${YELLOW}👉 Available commands: unit, integration, tests${NC}"
+    ;;
+esac

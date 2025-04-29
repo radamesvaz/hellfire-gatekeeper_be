529b9fb1cb6238deaa54229d9e10b08d3c9386f5
radamesvaz <rgvtovar@hotmail.com>
2025-04-29 14:34:57 -0400

    adding stock value to product

---
+++ migrations/000008_add_stock_value_to_product.up.sql
@@ -0,0 +1,5 @@
+UPDATE products SET stock = 6 WHERE id_product = 1;
+UPDATE products SET stock = 2 WHERE id_product = 2;
+
+UPDATE products_history SET stock = 6 WHERE id_product = 1;
+UPDATE products_history SET stock = 2 WHERE id_product = 2;

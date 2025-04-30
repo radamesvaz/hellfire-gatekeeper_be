0f0f8af43a1d5c428fbf1fea5830cf643e2b1c4f
radamesvaz <rgvtovar@hotmail.com>
2025-04-30 12:59:39 -0400

    adding delivery date and note to order table

---
+++ migrations/000009_add_note_time_column_to_order.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE orders DROP COLUMN note;
+ALTER TABLE orders DROP COLUMN delivery_date;
+
+ALTER TABLE orders_history DROP COLUMN note;
+ALTER TABLE orders_history DROP COLUMN delivery_date;
